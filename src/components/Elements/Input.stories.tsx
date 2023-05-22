3691b2d77f6fcfd7a75c3da114b050cc27ae0cb7
Renanmontanopaz <renanmontanopaz@outlook.com>
2023-05-21 22:54:33 -0300

    fezendo selects

--- src/components/Elements/Input.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import Input from './index';
-
-const stories = storiesOf('components/elements', module);
-stories.add(`Input`, () => <Input placeholder="what pokemon" />);
--- src/components/Pokedex/index.tsx
+++ src/components/Pokedex/index.tsx
@@ -1,7 +1,23 @@
-import {useEffect, useState} from "react";
+import  {useEffect, useState} from "react";
 import api from "../../services/api.ts";
 import {Pokemon, Request} from "../../models/Pokemon.ts";
 import styles from './index.module.css';
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import {
+    Box,
+    Checkbox,
+    Container,
+    FormControl,
+    InputLabel,
+    ListItemText,
+    MenuItem,
+    OutlinedInput,
+    Select,
+    SelectChangeEvent
+} from "@mui/material";
+import * as React from 'react';
 
 export const Pokedex = () =>{
 
@@ -22,6 +38,7 @@ export const Pokedex = () =>{
                     }
                 })
             )
+            console.log(payloadPokemons);
             setPokemons(payloadPokemons);
         }
         getAllPokemons()
@@ -35,12 +52,132 @@ export const Pokedex = () =>{
         }
     }
 
+    function SearchPokemon() {
+        return (
+            <Stack className={styles.input} spacing={3} sx={{ width: '80%', boxShadow: '4px 4px 16px rgba(1, 28, 64, 0.2)', borderRadius: '40px'}}>
+                <Autocomplete
+                    freeSolo
+                    id="free-solo-2-demo"
+                    disableClearable
+                    options={pokemons.map((option) => option.name)}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Encontre seu Pokemon"
+                            InputProps={{
+                                ...params.InputProps,
+                                type: 'search',
+                            }}
+                        />
+                    )}
+                />
+            </Stack>
+        );
+    }
 
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
+
+    const names = [
+        'Oliver Hansen',
+        'Van Henry',
+        'April Tucker',
+        'Ralph Hubbard',
+        'Omar Alexander',
+        'Carlos Abbott',
+        'Miriam Wagner',
+        'Bradley Wilkerson',
+        'Virginia Andrews',
+        'Kelly Snyder',
+    ];
+
+        const [personName, setPersonName] = React.useState<string[]>([]);
+
+        const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+            const {
+                target: { value },
+            } = event;
+            setPersonName(
+                // On autofill we get a stringified value.
+                typeof value === 'string' ? value.split('type') : value,
+            );
+        };
 
     return (
         <section className={styles.section}>
             <div className={styles.container}>
                 <h1 className={styles.h1}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
+                <SearchPokemon/>
+                <Box >
+                <FormControl sx={{ m: 1, width: 200 }}>
+                    <InputLabel id="demo-multiple-checkbox-label">Tipo</InputLabel>
+                    <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple
+                        value={personName}
+                        onChange={handleChange}
+                        input={<OutlinedInput label="Tag" />}
+                        renderValue={(selected) => selected.join('type')}
+                        MenuProps={MenuProps}
+                    >
+                        {pokemons.map((name) => (
+                            <MenuItem key={name.id} value={name.types.map((tipo) => tipo.type)}>
+                                <Checkbox checked={personName.indexOf(name.name) > -1} />
+                                <ListItemText primary={name.name} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <FormControl sx={{ m: 1, width: 200 }}>
+                    <InputLabel id="demo-multiple-checkbox-label">Tipo</InputLabel>
+                    <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple
+                        value={personName}
+                        onChange={handleChange}
+                        input={<OutlinedInput label="Tag" />}
+                        renderValue={(selected) => selected.join('type')}
+                        MenuProps={MenuProps}
+                    >
+                        {pokemons.map((name) => (
+                            <MenuItem key={name.id} value={name.types.map((tipo) => tipo.type)}>
+                                <Checkbox checked={personName.indexOf(name.name) > -1} />
+                                <ListItemText primary={name.name} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <FormControl sx={{ m: 1, width: 200 }}>
+                    <InputLabel id="demo-multiple-checkbox-label">Tipo</InputLabel>
+                    <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple
+                        value={personName}
+                        onChange={handleChange}
+                        input={<OutlinedInput label="Tag" />}
+                        renderValue={(selected) => selected.join('type')}
+                        MenuProps={MenuProps}
+                    >
+                        {pokemons.map((name) => (
+                            <MenuItem key={name.id} value={name.types.map((tipo) => tipo.type)}>
+                                <Checkbox checked={personName.indexOf(name.name) > -1} />
+                                <ListItemText primary={name.name} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                </Box>
             </div>
         </section>
     )
