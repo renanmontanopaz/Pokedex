153a06d7eb40d0fe6bf9a5248e567845cbb70139
renan montano Paz <renanmontanopaz@outlook.com>
2023-05-25 16:59:45 -0300

    laste commit

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,14 +5,14 @@ export const HomePokedex = ()=>{
     return (
         <section className={styles.section}>
             <div className={styles.container}>
-            <Grid container sx={{ justifyContent: 'space-between'}} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 11 }}>
+            <Grid container sx={{ justifyContent: 'space-between'}} spacing={{ xs: 3, md: 3 }} columns={{ xs: 4, sm: 8, md: 11 }}>
                 <Grid xs={2} sm={4} md={4}>
                     <h1><strong>Find</strong> all your favorite <strong>Pokemon</strong></h1>
                     <h2>You can know the type of pokemon, its strengths, disadvantages and abilities</h2>
                     <button>See Pokemons</button>
                 </Grid>
                 <Grid xs={7}>
-                    <CardMedia component="img" sx={{ width: 700}} image={banner}/>
+                    <CardMedia component="img" sx={{ width: 800}} image={banner}/>
                 </Grid>
             </Grid>
             </div>
